8546397aa93d8b29af1b88c12b23153c82bcab99
Zacchy01 <167019278+Zacchy01@users.noreply.github.com>
2025-04-15 11:54:46 +0100

    redirect.py

--- redirect.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, redirect
-
-app = Flask(__name__)
-
-@app.route('/')
-def go():
-    link = request.args.get('link', 'https://google.com')
-    return redirect(link, code=302)
